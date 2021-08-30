0126d976add468bc929241d9809dec18c216a7c5
Maxi Ibarra <maxi.8379@gmail.com>
2021-08-29 21:48:04 -0300

    entrega desafio8

--- desafio8/index.js
+++ desafio8/index.js
@@ -1,36 +1,55 @@
-const { json } = require('express');
-const express = require('express');
+
+const express = require("express");
 const fs = require("fs");
-const items = require('./productos.json')
-    // app.use(cors({origin:"*"}));
-    // app.use(express.json());
-    // app.use(express.text());
-// app.use(express.urlencoded({extended:true}));
+const items = require("./productos.json");
+
 
 
 const app = express();
-const server = app.listen(3000,() => {
-    console.log('escuchando en el puerto 3000');
+const server = app.listen(3000, () => {
+  console.log("escuchando en el puerto 3000");
 });
 
-app.get("/",(req,res)=>{
-    res.send("Hola!");
-})
-app.get('/api/productos/listar',(req,res)=>{
+app.get("/", (req, res) => {
+  res.send("Hola!");
+});
+app.get("/api/productos/listar", (req, res) => {
 
-    
-fs.promises.readFile('./productos.json').then(data => data.toString('utf-8')).then(datos =>{
-      
-    res.send(datos);
-})
+  fs.promises
+    .readFile("./productos.json")
+    .then((data) => data.toString("utf-8"))
+    .then((datos) => {
+      res.send(datos);
+    });
 });
 
-app.get('/api/productos/listar/:id',(req,res)=>{
-        
+app.get("/api/productos/listar/:id", (req, res) => {
+
+  if (!items[req.params.id]) {
+    res.json("producto no encontrado");
+  } else {
     res.send(items[req.params.id]);
-})
+  }
 
-// app.post('/api/productos/guardar',(req,res)=>{
+});
 
+app.post("/api/productos/guardar", (req, res) => {
 
-// })
+  try {
+    const item = req.body;
+    item.name = req.body.name;
+    item.precio = req.body.precio;
+    item.thumbnail = req.body.thumbnail;
+    item.id = items.length + 1;
+
+    fs.promises.readFile("./productos.json").then((data) => {
+      let datos = Json.parse(data);
+      datos.push(item);
+      fs.promises.writeFile("./productos.json", Json.stringify(datos), "utf-8");
+    });
+    return res.status(200).json(item);
+  } catch (error) {
+    return res.status(400).json({ error: "error al ingresar producto" });
+  }
+
+});
