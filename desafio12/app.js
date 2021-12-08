b0370e2f4060a695b1eedc074ed114d54c6ec52d
maximiliano ibarra <maxi.8379@gmail.com>
2021-12-07 22:47:03 -0300

    Desafio12

--- desafio12/app.js
+++ desafio12/app.js
@@ -1,45 +1,41 @@
-const express = require ('express');
+const express = require('express');
 const app = express();
-const http = require('http').Server(app);
+const http= require('http').Server(app);
 const io = require('socket.io')(http)
+
 const productos = [];
 
-
-app.use(express.urlencoded());
-app.use(express.json())
 app.use(express.static('./views'));
 
-app.get("/",(req,res)=>{
+app.get('/',(req,res)=>{
 
-    res.render("index",{productos});
+    res.send('index.html');
 })
 
-app.get("/productos",(req,res)=>{
-    
-  productos.push({...req.body})
+app.get('/productos',(req,res)=>{
 
-  res.render("index",{productos})
+    res.json(productos);
 
 })
 
 io.on('connection',(socket)=>{
-  console.log("cliente conectado:" + socket.id);
-    socket.emit('message',{productos})
+    console.log("cliente conectado:" + socket.id);
+        socket.emit('products',{productos})
 
-    socket.on('hello',(data)=>{
-      console.log(data);
-    })
-    socket.on('keyup',(data)=>{
-      console.log(data);
-      io.emit('user_keyup',{...data,id:socket.id});
+        socket.on('hello',(data)=>{
+            console.log(data);
+        })
 
-      productos.push({message:data.value,id:socket.id,nombre:socket.nombre,precio:socket.precio});
-    })
+        socket.on('keyup',(data)=>{
+            console.log(data);
+            io.emit('user_keyup',{...data,id:socket.id})
+        
+            productos.push({products:data.value,id:socket.id});
+        })
+})
 
-
-});
-
-
+app.use(express.urlencoded());
+app.use(express.json())
 app.set("views","./views");
 app.set("view engine","ejs");
 
--- desafio12/views/index.js
+++ desafio12/views/index.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded",function() {
     const socket= io(); 
-    const inp = document.querySelector("#nombre");
-    const Precio = document.querySelector("#precio");
+    const inp = document.querySelector("#producto");
+ 
     const myTemplate = document.querySelector("#myTemplate");
     const toRender = document.querySelector("#toRender");
     const productos = [];
 
-    socket.on('message',(data)=>{
+    socket.on('products',(data)=>{
         console.log(data);
     });
 
@@ -14,21 +14,14 @@ document.addEventListener("DOMContentLoaded",function() {
         console.log(data);
         productos.push(data);
         const template = ejs.compile(myTemplate.innerHTML);
-        toRender.innerHTML = template({items: productos,title:'Mi primera app Socket',nombre:productos.nombre,precio:productos.precio})
+        toRender.innerHTML = template({items:productos,title:'Mi primera app Socket'})
     }); 
 
     inp.addEventListener('keyup',(e)=>{
         if(e.keyCode == 13){
-            socket.emit('keyup',{nombre:e.target.nombre});
-            e.target.nombre = "";
-        } 
+            socket.emit('keyup',{nombre:e.target.value});
+            e.target.value = "";
+        }
     });
-    
-    Precio.addEventListener('keyup',(e)=>{
-        if(e.keyCode == 13){
-            socket.emit('keyup',{precio:e.target.precio});
-            e.target.precio = "";
-        } 
-    });
+ 
 });
-
