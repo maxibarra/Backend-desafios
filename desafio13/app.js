9f5cd3c1dd6badd043c4ec4e45a23d3fbc977b4b
maximiliano ibarra <maxi.8379@gmail.com>
2021-12-08 23:09:55 -0300

    Desafio13

--- desafio13/app.js
+++ desafio13/app.js
@@ -8,18 +8,14 @@ const productos = [
         id:1,
         producto:'celular',
         precio:132
-    },
-    {
-        id:2,
-        producto:'camara',
-        precio:132
-    },
-    {
-        id:3,
-        producto:'teclado',
-        precio:1325
     }
 ];
+const mensajes=[
+    {
+        user:'estapagina@gmail.com',
+        text:'en que lo puedo ayudar'
+    }
+]
 
 app.use(express.static('./views'));
 
@@ -28,35 +24,48 @@ app.get('/',(req,res)=>{
 
     res.send('index.html');
 })
+app.get('/mensajes',(req,res)=>{
+    res.json(mensajes);
+})
 
 app.get('/productos',(req,res)=>{
-
     res.json(productos);
-
 })
 
 io.on('connection',(socket)=>{
     console.log("cliente conectado:" + socket.id);
         socket.emit('products',productos)
 
-        socket.on('hello',(data)=>{
+        socket.emit('message',mensajes)
+
+        socket.on('hello',(data,data2)=>{
             console.log(data);
+            console.log(data2)
         })
 
-        socket.on('keyup',(data)=>{
+        socket.on('keyup',(data,data2)=>{
             console.log(data);
             io.emit('user_keyup',{...data,id:socket.id})
         
             productos.push({products:data.value,id:socket.id});
+
+            mensajes.push({message:data2.value});
         })
 
         socket.on('products',(data)=>{
+
+        })
+        socket.on('message',(data2)=>{
 
         })
 
         socket.on('new-product',(data)=>{
             productos.push(data)
             io.emit("products",productos)
+        })
+        socket.on('new-message',(data2)=>{
+            mensajes.push(data2)
+            io.emit("messages",mensajes)
         })
 })
 
--- desafio13/views/index.js
+++ desafio13/views/index.js
@@ -32,9 +32,42 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.querySelector("#products").innerHTML = html;
   };
+
+
+  const addMessage = () =>{
+    let message = {
+        user:document.querySelector('#username').value,
+        text:document.querySelector('#text').value,
+    };
+    console.log("mensaje enviado");
+    socket.emit('new-message',message)
+    return false;
+}
+
+document.querySelector('#chat').addEventListener("submit",function(e){
+    e.preventDefault();
+    addMessage();
+})
+const render2  = (data2)=> {
+
+    let msjs = data2.map(e=>
+        (`<div>
+        <em style="color:blue;font-weight:700;">${e.user}</em>:
+
+        <strong style="font-family:italic;color:green;"> ${e.text}</strong>
+        </div>
+        `
+        )).join(" ")
+
+        document.querySelector('#messages').innerHTML = msjs;
+}
   socket.on("products",(data) => {
     render(data);
   });
+
+  socket.on('messages',(data2)=>{
+    render2(data2);
+  })
 
   // socket.on("user_keyup",(data) => {
   //     console.log(data);
